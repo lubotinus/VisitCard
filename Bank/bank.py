86d62934fa7f75f5bc994b6fc9235477b791648d
Dmitry Barankov <dmitry.barankov@gmail.com>
2023-04-01 21:39:21 +0100

    After black

--- Bank/bank.py
+++ Bank/bank.py
@@ -5,46 +5,46 @@ from flask import Flask, request, redirect, render_template
 
 
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data=response.json()
+data = response.json()
 
-#print(data)
+# print(data)
 
-currency_data = data[0]['rates']
+currency_data = data[0]["rates"]
 
-print(currency_data[0]['currency'])
+print(currency_data[0]["currency"])
 
-data_file = open('currency.csv', 'w')
+data_file = open("currency.csv", "w")
 
 csv_writer = csv.writer(data_file)
 
 count = 0
 
 for emp in currency_data:
-	if count == 0:
+    if count == 0:
+        header = emp.keys()
+        csv_writer.writerow(header)
+        count += 1
 
-		header = emp.keys()
-		csv_writer.writerow(header)
-		count += 1
-
-	
-	csv_writer.writerow(emp.values())
+    csv_writer.writerow(emp.values())
 
 data_file.close()
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route("/", methods=["GET", "POST"])
 def set():
-	result = 0
-	if request.method=='POST':
-		
-		value = request.form.get('currency')
-		many = request.form.get('many')
-		print(value)
-		print(many)
-		result=f"{float(value)*float(many):.2f} PLN"
-	return render_template("form.html", result=result, data=currency_data, title="Currency calculator")
+    result = 0
+    if request.method == "POST":
+        value = request.form.get("currency")
+        many = request.form.get("many")
+        print(value)
+        print(many)
+        result = f"{float(value)*float(many):.2f} PLN"
+    return render_template(
+        "form.html", result=result, data=currency_data, title="Currency calculator"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=False)
